3fe78fb0ee733796dc9dcc9583c655a9edec9bf0
Cameron Paul <cpaul37@gmail.com>
2017-08-22 02:08:52 -0500

    Add thumbnail extractor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+use std::env;
+use std::ffi::CString;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    let source = CString::new(args.nth(1).unwrap()).unwrap();
+    let dest = CString::new(args.next().unwrap()).unwrap();
+
+    unsafe {
+        let data = libraw_init(0);
+        libraw_open_file(data, source.as_ptr());
+        libraw_unpack_thumb(data);
+        libraw_dcraw_thumb_writer(data, dest.as_ptr());
+    }
 }
